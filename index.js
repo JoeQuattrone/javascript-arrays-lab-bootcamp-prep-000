56946214b249f553cccc9455966e44bf4ffcc8e3
Joe Quattrone <joequattrone5@gmail.com>
2018-10-04 16:01:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  return kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten(name) {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  return kittens.shift(1)
+}
+
+function appendKitten(name) {
+  return [...kittens, "Broom"  ]
+}
+
+function prependKitten(name) {
+  return ["Arnold", ...kittens ]
+}
+
+function removeLastKitten() {
+  kittens.slice(-1)
+  return kittens
+}
